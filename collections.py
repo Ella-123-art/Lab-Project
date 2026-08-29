953ddecd623b824ea275c098b157a70be83ef1ca
Martey Emmanuella Odetsi <ellamartey1@gmail.com>
2026-08-29 23:34:56 +0000

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,24 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList)) 
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2]  = "orange"
+print(myFruitList)
+myFinalAnswerTurple = ("apple" , "banana", "pineapple")
+print(myFinalAnswerTurple)
+print(type(myFinalAnswerTurple))
+print(myFinalAnswerTurple[0])
+print(myFinalAnswerTurple[1])
+print(myFinalAnswerTurple[2])
+myFavoriteFruitDictionary = { 
+  "Akua" : "apple",         
+  "Saanvi" : "banana",      
+  "Paulo"  : "pineapple"     
+}                           
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
